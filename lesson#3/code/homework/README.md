032bcaa08e4b53b09fc6a9db0638721e332bc8a8
GyhzzZ <575858130@qq.com>
2021-01-17 12:56:15 +0800

    unfinish

---
+++ lesson#3/code/homework/README.md
@@ -0,0 +1,9 @@
+# substrate-lessonpro
+## lesson#3 homework
+#### 前端基于 [kitties-course 已有前端](https://github.com/SubstrateCourse/kitties-hw) 加以下 UX 及功能。这部份共 10 分:
+
+### 1. 能创建一个毛孩 (**3 分**)
+
+### 2. 每一个毛孩展示成一张卡片，並显示是不是属于你的 (**4 分**)
+
+### 3. 可以转让毛孩给另一位用户 (**3 分**)
---
+++ lesson#3/code/homework/frontend/src/Kitties.js
@@ -0,0 +1,89 @@
+import React, {useEffect, useState} from 'react';
+import {Form, Grid} from 'semantic-ui-react';
+
+import {useSubstrate} from './substrate-lib';
+import {TxButton} from './substrate-lib/components';
+
+import KittyCards from './KittyCards';
+
+export default function Kitties(props) {
+    const {api, keyring} = useSubstrate();
+    const {accountPair} = props;
+
+    const [kittyCnt, setKittyCnt] = useState(0);
+    const [kittyDNAs, setKittyDNAs] = useState([]);
+    const [kittyOwners, setKittyOwners] = useState([]);
+    const [kittyPrices, setKittyPrices] = useState([]);
+    const [kitties, setKitties] = useState([]);
+    const [status, setStatus] = useState('');
+
+    const fetchKittyCnt = () => {
+        /* TODO: 加代码，从 substrate 端读取数据过来 */
+        let unsubscribe;
+        api.query.kittiesModule.kittiesCount(get_count => {
+            console.log("kittie_count:", get_count.toString())
+            setKittyCnt(get_count.toNumber());
+        }).then(unsub => {
+            unsubscribe = unsub;
+        }).catch(console.error);
+
+        return () => unsubscribe && unsubscribe();
+    };
+
+    const fetchKitties = () => {
+        /* TODO: 加代码，从 substrate 端读取数据过来 */
+        let unsubscribe;
+
+        const all_index = Array.from(Array(kittyCnt), (v, k) => k);
+
+        api.query.kittiesModule.kitties.multi(all_index, all_kitties => {
+            api.query.kittiesModule.kittyOwners.multi(all_index, all_owner => {
+                api.query.kittiesModule.kittyPrices.multi(all_index, all_price => {
+                    all_kitties.forEach(function (item, index, arr) {
+                        arr[index].id = index;
+                        arr[index].dna = item.unwrap();
+                        arr[index].owner = keyring.encodeAddress(all_owner[index].unwrap());
+                        arr[index].price =  all_price[index].isEmpty?'未出售':all_price[index].unwrap();
+                    })
+                    console.log("all_kitties:", all_kitties)
+                    setKitties(all_kitties);
+                }).then(unsub => {
+                    unsubscribe = unsub;
+                }).catch(console.error);
+            }).then(unsub => {
+                unsubscribe = unsub;
+            }).catch(console.error);
+        }).then(unsub => {
+            unsubscribe = unsub;
+        }).catch(console.error);
+
+        return () => unsubscribe && unsubscribe();
+    };
+
+    const populateKitties = () => {
+        /* TODO: 加代码，从 substrate 端读取数据过来 */
+    };
+
+    useEffect(fetchKittyCnt, [api, keyring]);
+    useEffect(fetchKitties, [api, kittyCnt]);
+    useEffect(populateKitties, [kittyDNAs, kittyOwners]);
+
+    return <Grid.Column width={16}>
+        <h1>小毛孩-{kittyCnt}</h1>
+        <KittyCards kitties={kitties} accountPair={accountPair} setStatus={setStatus}/>
+        <Form style={{margin: '1em 0'}}>
+            <Form.Field style={{textAlign: 'center'}}>
+                <TxButton
+                    accountPair={accountPair} label='创建小毛孩11' type='SIGNED-TX' setStatus={setStatus}
+                    attrs={{
+                        palletRpc: 'kittiesModule',
+                        callable: 'create',
+                        inputParams: [],
+                        paramFields: []
+                    }}
+                />
+            </Form.Field>
+        </Form>
+        <div style={{overflowWrap: 'break-word'}}>{status}</div>
+    </Grid.Column>;
+}
